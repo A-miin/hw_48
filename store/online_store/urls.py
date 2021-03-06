c5b5cc85d17c8876e4522d3ecec5ca2f8e2fba9d
A-miin <zulaykaisaeva@gmail.com>
2021-03-06 18:33:25 +0600

    Add updating

--- store/online_store/urls.py
+++ store/online_store/urls.py
@@ -4,10 +4,14 @@ from .views import (
     index,
     product_view,
     product_create,
+    product_update,
+    product_delete
 )
 
 urlpatterns = [
     path('', index, name='product_list'),  # URL для отображения списка productov
     path('product/<int:pk>/', product_view, name = 'product_view'),
     path('product/create', product_create, name = 'product_create'),
+    path('product/<int:pk>/update', product_update, name = 'product_update'),
+    path('product/<int:pk>/delete', product_delete, name = 'product_delete'),
 ]
--- store/online_store/views.py
+++ store/online_store/views.py
@@ -36,3 +36,32 @@ def product_create(request):
             return redirect('product_view',product.id)
         else:
             return render(request, 'product_create.html', context={'form':form})
+
+def product_update(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    if request.method=='GET':
+        form = ProductForm(
+            initial={
+                'name':product.name,
+                'category':product.category,
+                'description':product.description,
+                'remainder':product.remainder,
+                'price':product.price
+            }
+        )
+        return render(request,'product_update.html',context={'form':form, 'product':product})
+    elif request.method=='POST':
+        form=ProductForm(data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data.get('name')
+            product.category = form.cleaned_data.get('category')
+            product.description = form.cleaned_data.get('description')
+            product.remainder = form.cleaned_data.get('remainder')
+            product.price = form.cleaned_data.get('price')
+            product.save()
+            return redirect('product_view', product.id)
+        else:
+            return render(request, 'product_update.html', context={'form':form, 'product':product})
+
+def product_delete(request, pk):
+    pass
