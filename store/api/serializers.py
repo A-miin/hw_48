594f802a9e1d896b3aefb3a21629c6f66658d678
A-miin <zulaykaisaeva@gmail.com>
2021-05-27 18:52:37 +0600

    add create def for orderproduct

--- store/api/serializers.py
+++ store/api/serializers.py
@@ -20,3 +20,10 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ('user', 'name', 'tel', 'address','created_at', 'products')
+
+    def create(self, validated_data):
+        products = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+        for product in products:
+            ProductOrder.objects.create(order=order, **product)
+        return order
